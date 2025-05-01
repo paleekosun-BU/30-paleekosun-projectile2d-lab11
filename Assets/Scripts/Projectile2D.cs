19931f76565ec7780de0edf03a689c8a93c56300
paleekosun-BU <paleekosun.wibo@bumail.net>
2025-05-01 21:41:04 +0700

    AddScript

---
+++ Assets/Scripts/Projectile2D.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Projectile2D : MonoBehaviour
+{
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private GameObject target;
+    [SerializeField] private Rigidbody2D bulletPrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit " + hit.collider.name);
+            }
+        }
+    }
+
+    Vector2 CalculateProjectile(Vector2 origin, Vector2 target, float time)
+    {
+        Vector2 distance = target - origin;
+
+        float velocityX = distance.x / time;
+        float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+
+        Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+
+        return projectileVelocity;
+
+
+    }
+}
